c5d307055cbb9db1a28bfd9f602b93d3c28229ef
Kingburger5 <128675682+Kingburger5@users.noreply.github.com>
2026-05-29 16:16:14 +1200

    Add files via upload

---
+++ Assesment1_Task1.py
@@ -0,0 +1,6 @@
+m = float(input("Enter the mass of the object in kg: "))
+v = float(input("Enter the velocity of the object in m/s: "))
+
+p = m * v
+
+print("The momentum of the object is:", p, "kg*m/s")
